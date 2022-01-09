135084af391cb5bfc290cc0b9ccd3bcd1c85e0f8
navidem <navid.emamdoost@gmail.com>
2022-01-09 22:31:49 +0000

    running all.sh conditionally

--- oss_fuzz_integration/build_patched_oss_fuzz.sh
+++ oss_fuzz_integration/build_patched_oss_fuzz.sh
@@ -35,5 +35,8 @@ rm -rf ./oss-fuzz/infra/base-images/base-builder/post-processing
 cp -rf ../llvm ./oss-fuzz/infra/base-images/base-clang/llvm
 cp -rf ../post-processing ./oss-fuzz/infra/base-images/base-builder/post-processing
 
-cd oss-fuzz
-./infra/base-images/all.sh
+# Skip all.sh if it is in cloud build.
+if [[ $CLOUD_BUILD_ENV != "1" ]]; then
+   cd oss-fuzz
+   ./infra/base-images/all.sh
+fi
